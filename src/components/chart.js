ab8a4aa46573f3c92af6f5a5db4e2c0ffac27797
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-03-16 23:16:23 -0500

    chart added

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,7 +1,21 @@
 import React from "react";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 const Chart = () => {
-  return <div>I'm a chart</div>;
+  return (
+    <div className="chartbox">
+      <TradingViewWidget
+        symbol="BITFINEX:BTCUSD"
+        interval="D"
+        timezone="Etc/UTC"
+        theme={Themes.DARK}
+        style="1"
+        locale="en"
+        width="100%"
+        height="400"
+      />
+    </div>
+  );
 };
 
 export default Chart;
