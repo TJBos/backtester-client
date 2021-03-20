ea8554d722db20ad1cfd6363c166bf20476c02a9
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-03-20 15:10:20 -0500

    building out the form

--- src/components/form.js
+++ src/components/form.js
@@ -4,10 +4,32 @@ import "../styles/form.scss";
 const Form = () => {
   const [parameters, setParameters] = React.useState({});
 
+  const handleChange = (event) => {
+    setParameters({ ...parameters, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="form-box">
       <form>
-        <input type="text" placeholder="interval"></input>
+        <label for="interval">Interval:</label>
+        <select id="interval" name="interval" onChange={handleChange}>
+          <option value="1D">1D</option>
+          <option value="4H">4H</option>
+          <option value="1H">1H</option>
+          <option value="1M">1M</option>
+        </select>
+        <br></br>
+        <label for="indicator">Indicator:</label>
+        <select id="indicator" name="indicator" onChange={handleChange}>
+          <option value="" disabled selected>
+            Select indicator
+          </option>
+          <option value="RSI">RSI</option>
+          <option value="MACD">MACD</option>
+          <option value="Price">Price</option>
+          <option value="1M">1M</option>
+        </select>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
