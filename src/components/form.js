0b7dcad34b4d2280127c3417a6c42c2c11e03888
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-03-21 22:19:59 -0500

    added buy signals

--- src/components/form.js
+++ src/components/form.js
@@ -8,28 +8,88 @@ const Form = () => {
     setParameters({ ...parameters, [event.target.name]: event.target.value });
   };
 
+  const show = () => {
+    if (parameters.buyIndicator2 === "Number") {
+      return "number";
+    } else {
+      return "hidden";
+    }
+  };
+
   return (
     <div className="form-box">
+      <h1>Stack your strategy</h1>
       <form>
         <label for="interval">Interval:</label>
-        <select id="interval" name="interval" onChange={handleChange}>
+        <select
+          id="interval"
+          name="interval"
+          className="selection"
+          onChange={handleChange}
+        >
+          <option value="" disabled selected>
+            Select interval
+          </option>
           <option value="1D">1D</option>
           <option value="4H">4H</option>
           <option value="1H">1H</option>
           <option value="1M">1M</option>
         </select>
         <br></br>
-        <label for="indicator">Indicator:</label>
-        <select id="indicator" name="indicator" onChange={handleChange}>
+        <label for="buy-signal">Buy signal:</label> Buy when:
+        <select
+          id="buy-signal"
+          name="buyIndicator1"
+          className="selection"
+          onChange={handleChange}
+        >
           <option value="" disabled selected>
-            Select indicator
+            Select option
           </option>
           <option value="RSI">RSI</option>
-          <option value="MACD">MACD</option>
+          <option value="EMA20">MACD</option>
           <option value="Price">Price</option>
-          <option value="1M">1M</option>
+          <option value="EMA50">1M</option>
         </select>
-        <button type="submit">Submit</button>
+        is
+        <select
+          id="buy-signal"
+          name="buyAction"
+          className="selection"
+          onChange={handleChange}
+        >
+          <option value="" disabled selected>
+            Select action
+          </option>
+          <option value="smaller">Smaller</option>
+          <option value="larger">Larger</option>
+        </select>{" "}
+        than
+        <select
+          id="buy-signal"
+          name="buyIndicator2"
+          className="selection"
+          onChange={handleChange}
+        >
+          <option value="" disabled selected>
+            Select option
+          </option>
+          <option value="RSI">RSI</option>
+          <option value="EMA20">MACD</option>
+          <option value="Price">Price</option>
+          <option value="Number">A number</option>
+        </select>
+        <input
+          type={show()}
+          className="selection"
+          name="numberEntered"
+          placeholder="which number?"
+          onChange={handleChange}
+        ></input>
+        <br></br>
+        <button id="sub-button" type="submit">
+          Backtest!
+        </button>
       </form>
     </div>
   );
