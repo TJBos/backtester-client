c550c1b1883a8d0fa2b0f9facbc8c6497ed9ab7c
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-03-31 22:47:50 -0500

    init flask api

---
+++ flask_API/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    response = { 'item': 'number1', 'another': 4 }
+    return response
+
+@app.route('/add', methods=['POST'])
+def add():
+    print(request.json["name"])
+    return { "response": "OK "}
+
+if __name__ == "__main__":
+    app.run(debug=True)
