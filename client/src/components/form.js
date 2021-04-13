d9e4ff274e646d255fdf0b8e7c663fadd3e5b99a
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-04-12 21:16:39 -0500

    added a modal

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,9 +1,38 @@
 import React from "react";
 import "../styles/form.scss";
 import BuySignal from "./buysignal";
+import { Modal, Button } from "react-bootstrap";
 
 const Form = () => {
-  <BuySignal />;
+  const [showBuy, setShowBuy] = React.useState(false);
+  const handleShow = () => {
+    setShowBuy(true);
+  };
+  const handleClose = () => {
+    setShowBuy(false);
+  };
+
+  return (
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Add Buy Signal
+      </Button>
+      <Modal show={showBuy} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default Form;
